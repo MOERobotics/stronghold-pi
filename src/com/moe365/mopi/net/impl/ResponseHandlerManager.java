27b5dd312f2088e28ccae35937a8c174699dbeb0
mailmindlin <liamttlyrox@gmail.com>
2016-08-22 07:35:46 -0400

    Added ResponseHandlerManager
    TODO come up with a better name. SomethingManagers are not very
    descriptive

---
+++ src/com/moe365/mopi/net/impl/ResponseHandlerManager.java
@@ -0,0 +1,126 @@
+package com.moe365.mopi.net.impl;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import com.moe365.mopi.net.channel.DataChannelClient;
+import com.moe365.mopi.net.packet.DataPacket;
+
+public class ResponseHandlerManager implements Runnable {
+	/**
+	 * Maximum time (in ms) between calls to {@link #cleanup()}.
+	 */
+	public static final long MAX_CLEANUP_WAIT = 10_000;
+	ConcurrentHashMap<Integer, ResponseHandler> handlers = new ConcurrentHashMap<>();
+	PriorityBlockingQueue<ResponseHandler> queue = new PriorityBlockingQueue<>(10, (a,b)->(a.getInvalidationTime().compareTo(b.getInvalidationTime())));
+	public <T extends DataPacket> CompletableFuture<T> onAcknowledgement(int channel, int id, Duration timeout) {
+		//TODO finish
+		return null;
+	}
+	public long cleanup() {
+		ResponseHandler handler;
+		Instant now = Instant.now();
+		while ((handler = queue.poll()) != null) {
+			Instant invalidationTime = handler.getInvalidationTime();
+			if (invalidationTime.isAfter(now)) {
+				queue.offer(handler);
+				try {
+					return Math.min(Duration.between(now, invalidationTime).toMillis(), MAX_CLEANUP_WAIT);
+				} catch (ArithmeticException e) {
+					return MAX_CLEANUP_WAIT;
+				}
+			}
+			int[] ids = handler.getIds();
+			for (int i = 0; i < ids.length; i++)
+				handlers.remove(ids[i]);
+			//TODO possibly move execution to another thread
+			handler.onTimeout();
+		}
+		return MAX_CLEANUP_WAIT;
+	}
+	public void handle(DataChannelClient client, DataPacket packet) {
+		ResponseHandler handler = handlers.get(packet.getAckId());
+		if (handler != null) {
+			if (handler.getInvalidationTime().isAfter(Instant.now())) {
+				//Handler is invalid, so remove it.
+				handlers.remove(packet.getAckId());
+				queue.remove(handler);
+				return;
+			}
+			handler.onResponse(client, packet);
+			if (handler.doRemoveAfterResponse()) {
+				handlers.remove(packet.getAckId());
+				queue.remove(handler);
+			}
+		}
+	}
+	public void addHandler(ResponseHandler handler) {
+		int[] ids = handler.getIds();
+		for (int i = 0; i < ids.length; i++)
+			handlers.put(ids[i], handler);
+		queue.offer(handler);
+	}
+	/**
+	 * Periodically run {@link #cleanup()}
+	 */
+	@Override
+	public void run() {
+		while (!Thread.interrupted()) {
+			try {
+				Thread.sleep(cleanup());
+			} catch (InterruptedException e) {
+				return;
+			}
+		}
+	}
+	public static abstract class ResponseHandler {
+		public static ResponseHandler with(int[] ids, Instant invalidationTime, boolean removeAfterResponse, Consumer<DataPacket> responseHandler, Runnable onTimeout) {
+			return new ResponseHandler(ids, invalidationTime, removeAfterResponse) {
+				@Override
+				public void onTimeout() {
+					onTimeout.run();
+				}
+				@Override
+				public void onResponse(DataChannelClient client, DataPacket packet) {
+					responseHandler.accept(packet);
+				}
+			};
+		}
+		public static ResponseHandler with(int[] ids, Instant invalidationTime, boolean removeAfterResponse, BiConsumer<DataChannelClient, DataPacket> responseHandler, Runnable onTimeout) {
+			return new ResponseHandler(ids, invalidationTime, removeAfterResponse) {
+				@Override
+				public void onTimeout() {
+					onTimeout.run();
+				}
+				@Override
+				public void onResponse(DataChannelClient client, DataPacket packet) {
+					responseHandler.accept(client, packet);
+				}
+			};
+		}
+		protected final int[] ids;
+		protected final Instant invalidationTime;
+		protected final boolean removeAfterResponse;
+		public ResponseHandler(int[] ids, Instant invalidationTime, boolean removeAfterResponse) {
+			this.ids = ids;
+			this.invalidationTime = invalidationTime;
+			this.removeAfterResponse = removeAfterResponse;
+		}
+		public Instant getInvalidationTime() {
+			return invalidationTime;
+		}
+		public int[] getIds() {
+			return ids;
+		}
+		public boolean doRemoveAfterResponse() {
+			return this.removeAfterResponse;
+		}
+		public abstract void onTimeout();
+		public abstract void onResponse(DataChannelClient client, DataPacket packet);
+	}
+}
