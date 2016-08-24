9e50b14ef92ef7633870d0d41cf6f82726b8e20f
mailmindlin <liamttlyrox@gmail.com>
2016-08-23 20:17:47 -0400

    Fixed line wrapping, allocation
    Allocating a StringBuffer when it's instantiated correctly can speed up
    performance by a lot (microbenchmarks back me up)

--- src/com/moe365/mopi/util/StringUtils.java
+++ src/com/moe365/mopi/util/StringUtils.java
@@ -3,11 +3,11 @@ package com.moe365.mopi.util;
 public class StringUtils {
 	public static final char[] hexChars = "0123456789ABCDEF".toCharArray();
 	public static String toHexString(byte[] buf, int offset, int length, int width) {
-		StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder(length * 2 + length/width + 1);
 		for (int i = 0; i < length; i++) {
 			byte b = buf[offset + i];
 			sb.append(hexChars[b>>>4]).append(hexChars[b&0x0F]).append(' ');
-			if (i % width == 0)
+			if (i % width == width - 1)
 				sb.append('\n');
 		}
 		return sb.toString();
