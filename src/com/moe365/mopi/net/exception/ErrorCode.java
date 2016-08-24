917e0056c6ba2b8b8b9a36cd8519d4d6a731b6db
mailmindlin <liamttlyrox@gmail.com>
2016-08-23 21:07:59 -0400

    Expanded error stuff

--- src/com/moe365/mopi/net/exception/ErrorCode.java
+++ src/com/moe365/mopi/net/exception/ErrorCode.java
@@ -1,8 +1,11 @@
 package com.moe365.mopi.net.exception;
 
 public enum ErrorCode {
+	INVALID_CHANNEL,
 	UNKNOWN_PACKET_TYPE,
 	MALFORMED_PACKET,
-	NOT_FOUND,
-	
+	INTERNAL_ERROR,
+	NOT_SUBSCRIBED_TO_CHANNEL,
+	//Differs from UNKNOWN_'' b/c this one knows what type of packet it is, but just doesn't like it.
+	ILLEGAL_PACKET_TYPE,
 }
--- src/com/moe365/mopi/net/packet/ErrorPacket.java
+++ src/com/moe365/mopi/net/packet/ErrorPacket.java
@@ -3,6 +3,43 @@ package com.moe365.mopi.net.packet;
 import com.moe365.mopi.net.exception.ErrorCode;
 @PacketTypeCode(PacketTypeCode.ERROR)
 public interface ErrorPacket extends DataPacket {
-	ErrorCode getErrorCode();
-	String getErrorMessage();
+	ErrorCode getCode();
+	String getMessage();
+	public static class MutableErrorPacket extends AbstractMutableDataPacket implements ErrorPacket {
+
+		public MutableErrorPacket() {
+			
+		}
+		public MutableErrorPacket(ErrorCode code, String message) {
+			
+		}
+		@Override
+		public int getLength() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public ErrorCode getCode() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		public MutableErrorPacket setCode(ErrorCode code) {
+			//TODO finish
+			return this;
+		}
+
+		@Override
+		public String getMessage() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		public MutableErrorPacket setMessage(String message) {
+			//TODO finish
+			return this;
+		}
+		
+	}
 }
