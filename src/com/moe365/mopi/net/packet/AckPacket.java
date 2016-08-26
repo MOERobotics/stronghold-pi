3ec1767481e446898229f3b34194cff38561167f
mailmindlin <liamttlyrox@gmail.com>
2016-08-26 00:29:10 -0400

    Formatting

--- src/com/moe365/mopi/net/packet/AckPacket.java
+++ src/com/moe365/mopi/net/packet/AckPacket.java
@@ -1,11 +1,15 @@
 package com.moe365.mopi.net.packet;
+
+import java.nio.ByteBuffer;
+
 /**
  * <p>
- * A packet that acknowledges that another has recieved,
- * explicitly declaring that no error has occurred as a result
- * of any action invoked by the previous packet.
+ * A packet that acknowledges that another has recieved, explicitly declaring
+ * that no error has occurred as a result of any action invoked by the previous
+ * packet.
  * </p>
  * No <var>ACK</var> packet should be used to reply to an ACK packet.
+ * 
  * @author mailmindlin
  */
 @PacketTypeCode(PacketTypeCode.ACK)
