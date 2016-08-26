8698a24e0aa74b59370652a356c0d6c40b45adea
mailmindlin <liamttlyrox@gmail.com>
2016-08-26 00:30:26 -0400

    Implemented ErrorPacket

--- src/com/moe365/mopi/net/packet/ErrorPacket.java
+++ src/com/moe365/mopi/net/packet/ErrorPacket.java
@@ -1,43 +1,67 @@
 package com.moe365.mopi.net.packet;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
 import com.moe365.mopi.net.exception.ErrorCode;
+
 @PacketTypeCode(PacketTypeCode.ERROR)
 public interface ErrorPacket extends DataPacket {
 	ErrorCode getCode();
+
 	String getMessage();
+
 	public static class MutableErrorPacket extends AbstractMutableDataPacket implements ErrorPacket {
+		ErrorCode code;
+		String message;
+		int length = 0;
 
 		public MutableErrorPacket() {
-			
+			this.setTypeCode(PacketTypeCode.ERROR);
 		}
+
+		public MutableErrorPacket(ByteBuffer buf) {
+			super(buf);
+			this.code = ErrorCode.values()[buf.get() & 0xFF];
+			int msgLen = ((buf.get() & 0xFF) << 12) | (buf.getShort() & 0xFF_FF);
+			byte[] data = new byte[msgLen];
+			buf.get(data, 0, msgLen);
+			this.message = new String(data, StandardCharsets.UTF_8);
+			this.length = DataPacket.HEADER_LENGTH + 4 + msgLen;
+			this.setTypeCode(PacketTypeCode.ERROR);
+		}
+
 		public MutableErrorPacket(ErrorCode code, String message) {
-			
+			this();
+			this.code = code;
+			this.message = message;
 		}
+
 		@Override
 		public int getLength() {
-			// TODO Auto-generated method stub
-			return 0;
+			if (length < 1)
+				length = DataPacket.HEADER_LENGTH + 4 + (message == null ? 0 : message.getBytes(StandardCharsets.UTF_8).length);
+			return length;
 		}
 
 		@Override
 		public ErrorCode getCode() {
-			// TODO Auto-generated method stub
-			return null;
+			return this.code;
 		}
-		
+
 		public MutableErrorPacket setCode(ErrorCode code) {
-			//TODO finish
+			this.code = code;
 			return this;
 		}
 
 		@Override
 		public String getMessage() {
-			// TODO Auto-generated method stub
-			return null;
+			return this.message;
 		}
-		
+
 		public MutableErrorPacket setMessage(String message) {
-			//TODO finish
+			this.message = message;
+			this.length = 0;
 			return this;
 		}
 
