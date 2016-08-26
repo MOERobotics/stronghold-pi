557ae584b2e1e39e36da78b2fc6e70a6c75a5973
mailmindlin <liamttlyrox@gmail.com>
2016-08-26 00:33:56 -0400

    Implemented eventful datachannel for ws

---
+++ src/com/moe365/mopi/net/impl/EventfulWsDataChannel.java
@@ -0,0 +1,67 @@
+package com.moe365.mopi.net.impl;
+
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import com.moe365.mopi.net.channel.DataChannelClient;
+import com.moe365.mopi.net.channel.EventfulDataChannel;
+import com.moe365.mopi.net.channel.UnsubscriptionReason;
+import com.moe365.mopi.net.packet.DataPacket;
+
+public class EventfulWsDataChannel extends AbstractWsDataChannel implements EventfulDataChannel {
+
+	protected Set<Consumer<DataChannelClient>> subscriptionHandlers;
+	protected Set<BiConsumer<DataChannelClient, UnsubscriptionReason>> unsubscriptionHandlers;
+	protected Set<BiConsumer<DataPacket, DataChannelClient>> packetRecievedHandlers;
+
+	@Override
+	public void addSubscriptionHandler(Consumer<DataChannelClient> handler) {
+		this.subscriptionHandlers.add(handler);
+	}
+
+	@Override
+	public boolean removeSubscriptionHandler(Consumer<DataChannelClient> handler) {
+		return this.subscriptionHandlers.remove(handler);
+	}
+
+	@Override
+	public void addUnsubscriptionHandler(BiConsumer<DataChannelClient, UnsubscriptionReason> handler) {
+		this.unsubscriptionHandlers.add(handler);
+	}
+
+	@Override
+	public boolean removeUnsubscriptionHandler(BiConsumer<DataChannelClient, UnsubscriptionReason> handler) {
+		return this.unsubscriptionHandlers.remove(handler);
+	}
+
+	@Override
+	public void addRecievePacketHandler(BiConsumer<DataPacket, DataChannelClient> handler) {
+		this.packetRecievedHandlers.add(handler);
+	}
+
+	@Override
+	public boolean removeRecievePacketHandler(BiConsumer<DataPacket, DataChannelClient> handler) {
+		return this.packetRecievedHandlers.remove(handler);
+	}
+
+	@Override
+	protected boolean onSubscription(DataChannelClient client) {
+		//TODO change threading?
+		for (Consumer<DataChannelClient> handler : this.subscriptionHandlers)
+			handler.accept(client);
+		return true;
+	}
+
+	@Override
+	protected void onRecievePacket(DataPacket packet, DataChannelClient client) {
+		for (BiConsumer<DataPacket, DataChannelClient> handler : this.packetRecievedHandlers)
+			handler.accept(packet, client);
+	}
+
+	@Override
+	protected void onUnsubscription(DataChannelClient client, UnsubscriptionReason reason) {
+		for (BiConsumer<DataChannelClient, UnsubscriptionReason> handler : this.unsubscriptionHandlers)
+			handler.accept(client, reason);
+	}
+}
