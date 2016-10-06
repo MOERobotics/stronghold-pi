5a41985a0b13e207688ad43cf5862f4086a91b20
mailmindlin <liamttlyrox@gmail.com>
2016-10-06 15:43:30 -0400

    Changed to relative reads

--- src/com/moe365/mopi/net/packet/MutableWrappingDataPacket.java
+++ src/com/moe365/mopi/net/packet/MutableWrappingDataPacket.java
@@ -7,26 +7,26 @@ public class MutableWrappingDataPacket extends AbstractWrappedDataPacket impleme
 		super(origin.writeTo(ByteBuffer.allocate(origin.getLength())));
 	}
 	public MutableWrappingDataPacket(ByteBuffer buf) {
-		super(buf);
+		super(buf.duplicate());
 	}
 	@Override
 	public MutableWrappingDataPacket setId(int id) {
-		super.buf.putInt(DataPacket.CHANNEL_ID_OFFSET, id);
+		super.buf.putInt(buf.position() + DataPacket.CHANNEL_ID_OFFSET, id);
 		return this;
 	}
 	@Override
 	public MutableWrappingDataPacket setAckId(int ack) {
-		super.buf.putInt(DataPacket.ACK_PACKET_ID_OFFSET, ack);
+		super.buf.putInt(buf.position() + DataPacket.ACK_PACKET_ID_OFFSET, ack);
 		return this;
 	}
 	@Override
 	public MutableWrappingDataPacket setChannelId(int channel) {
-		super.buf.putShort(DataPacket.CHANNEL_ID_OFFSET, (short) channel);
+		super.buf.putShort(buf.position() + DataPacket.CHANNEL_ID_OFFSET, (short) channel);
 		return this;
 	}
 	@Override
 	public MutableWrappingDataPacket setTypeCode(int type) {
-		super.buf.putShort(DataPacket.TYPE_CODE_OFFSET, (short) type);
+		super.buf.putShort(buf.position() + DataPacket.TYPE_CODE_OFFSET, (short) type);
 		return this;
 	}
 }
