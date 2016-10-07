a2fc70c49d11f8b58871da72022558b840d6641c
mailmindlin <liamttlyrox@gmail.com>
2016-10-06 20:15:56 -0400

    Simplified new processing algorithm

--- src/com/moe365/mopi/BoundingBoxThing.java
+++ src/com/moe365/mopi/BoundingBoxThing.java
@@ -1,130 +1,102 @@
 package com.moe365.mopi;
 
-import java.awt.Rectangle;
 import java.util.List;
 
 import com.moe365.mopi.geom.PreciseRectangle;
 
 public class BoundingBoxThing {
 	/**
-	 * Smallest allowable dimension for any side of the box.
-	 * Increasing this value will increase the speed of searching through an image,
-	 * while decreasing it will find more boxes
+	 * Smallest allowed width of a bounding box, in pixels.
+	 * 
+	 * Decreasing the value of this constant will find smaller blobs,
+	 * but will be more computationally expensive.
 	 */
 	private static final int MIN_WIDTH = 40;
+	/**
+	 * Smallest allowed height of a bounding box.
+	 * @see #MIN_WIDTH
+	 */
 	private static final int MIN_HEIGHT = 40;
 	
+	/**
+	 * Find the power of two greater or equal to the value 
+	 * @param value
+	 * @return
+	 */
 	private static final int nextPowerOf2(final int value) {
-		final int tmp = value - 1;
-		return (tmp >>> 16 | tmp >>> 8 | tmp >>> 4 | tmp >>> 2 | tmp >>> 1) + 1;
+		return 1 << (32 - Integer.numberOfLeadingZeros(value - 1));
 	}
 	
-	public static boolean boundingBoxRecursive(boolean[][] img, List<PreciseRectangle> bbr, final int xMin, final int xMax,
+	/**
+	 * Searches an image for blobs. You can think of it as a kind of binary search
+	 * of a 2d array.
+	 * @param img A boolean image, ordered row, column
+	 * @param result List to populate with bounding boxes
+	 * @param xMin Left bound of image to search (minimum index of the array)
+	 * @param xMax Right bound of the image to search (maximum index of the array)
+	 * @param yMin Top bound
+	 * @param yMax Bottom bound
+	 * @return
+	 */
+	public static boolean boundingBox(boolean[][] img, List<PreciseRectangle> result, final int xMin, final int xMax,
 			final int yMin, final int yMax) {
 		int width = xMax - xMin;
 		int height= yMax - yMin;
 		if (width < MIN_WIDTH || height < MIN_HEIGHT)
-			// The image is too small to possibly find any boxes that are larger than MINDIM
-			// in width and height
+			// The image is too small to find any boxes
 			return false;
+		int xSplit = -2;
+		int ySplit = -2;
+		//It should be faster to calculate a split perpendicular to the widest axis
 		if (width >= height) {
-			//Compute smallest power of two greater than width
-			int step = nextPowerOf2(width);
-			while (step > 2) {
-				//TODO optimize w/ bit twiddling
-				outer:
-				for (int split = xMin + (step / 2); split < xMax; split += step) {
-					for (int y = yMin; y < yMax; y++)
-						if (test(img, xMin + split, y))
-							continue outer;
-					return boundingBoxRecursive0(img, bbr, split + 1, xMax, yMin, yMax) | boundingBoxRecursive0(img, bbr, xMin, split - 1, yMin, yMax);
-				}
-				step /= 2;
-			}
-			//Complete for top/bottom
-			return boundingBoxRecursiveH(img, bbr, xMin, xMax, yMin, yMax);
+			if ((xSplit = splitV(img, xMin, xMax, yMin, yMax)) < 0)
+				ySplit = splitH(img, xMin, xMax, yMin, yMax);
+		} else {
+			if ((xSplit = splitV(img, xMin, xMax, yMin, yMax)) < 0)
+				ySplit = splitH(img, xMin, xMax, yMin, yMax);
 		}
-		int step = nextPowerOf2(height);
-		while (step > MIN_HEIGHT) {
-			outer:
-			for (int split = yMin + (step / 2); split < yMax; split += step) {
-				for (int x = 0; x < height; x++)
-					if (test(img, xMin + x, yMin + split))
-						continue outer;
-				return boundingBoxRecursive0(img, bbr, xMin, xMax, split + 1, yMax) | boundingBoxRecursive0(img, bbr, xMin, xMax, yMin, split - 1);
-			}
-			step /= 2;
-		}
-		return boundingBoxRecursiveW(img, bbr, xMin, xMax, yMin, yMax);
-	}
-	/**
-	 * Called after the upper/lower bounds are known
-	 * @param img
-	 * @param bbr
-	 * @param xMin
-	 * @param xMax
-	 * @param yMin
-	 * @param yMax
-	 * @return
-	 */
-	private static boolean boundingBoxRecursiveW(boolean[][] img, List<PreciseRectangle> bbr, final int xMin, final int xMax,
-			final int yMin, final int yMax) {
-		final int width = xMax - xMin;
-		final int height= yMax - yMin;
-		if (width < MIN_WIDTH || height < MIN_WIDTH)
-			// The image is too small to possibly find any boxes that are larger than MINDIM
-			// in width and height
-			return false;
-		//Compute smallest power of two greater than width
-		int step = nextPowerOf2(width);
-		while (step > 2) {
-			//TODO optimize w/ bit twiddling
-			outer:
-			for (int split = xMin + (step / 2); split < xMax; split += step) {
-				for (int y = yMin; y < yMax; y++)
-					if (test(img, xMin + split, y))
-						continue outer;
-				return boundingBoxRecursive0(img, bbr, split + 1, xMax, yMin, yMax) | boundingBoxRecursive0(img, bbr, xMin, split - 1, yMin, yMax);
-			}
-			step /= 2;
-		}
-		return bbr.add(new PreciseRectangle(xMin, xMax, yMin, yMax));
+		if (xSplit >= 0)
+			return boundingBox(img, result, xMin, xSplit - 1, yMin, yMax) | boundingBox(img, result, xSplit + 1, xMax, yMin, yMax);
+		if (ySplit >= 0)
+			return boundingBox(img, result, xMin, xMax, yMin, ySplit - 1) | boundingBox(img, result, xMin, xMax, ySplit + 1, yMax);
+		return result.add(new PreciseRectangle(xMin, xMax, yMin, yMax));
 	}
 	
-	private static boolean boundingBoxRecursiveH(boolean[][] img, List<PreciseRectangle> bbr, final int xMin, final int xMax,
-			final int yMin, final int yMax) {
-		final int width = xMax - xMin;
-		final int height= yMax - yMin;
-		if (width < MIN_WIDTH || height < MIN_HEIGHT)
-			// The image is too small to possibly find any boxes that are larger than MINDIM
-			// in width and height
-			return false;
-		//Compute smallest power of two greater than width
-		int step = nextPowerOf2(height);
+	/**
+	 * Try to split the image horizontally (perpendicular to the Y axis)
+	 * @param img Image
+	 * @param xMin Left bound of search area
+	 * @param xMax Right bound of search area
+	 * @param yMin Bottom of search area
+	 * @param yMax Top of search area
+	 * @return The index that can be split at, or -1 if no split is found
+	 */
+	private static final int splitH(boolean[][] img, final int xMin, final int xMax, final int yMin, final int yMax) {
+		int step = nextPowerOf2(yMax - yMin);
 		while (step > 2) {
-			//TODO optimize w/ bit twiddling
-			outer:
-			for (int split = yMin + (step / 2); split < yMax; split += step) {
+			outer: for (int split = yMin + step / 2; split < yMax; split += step) {
 				for (int x = xMin; x < xMax; x++)
-					if (test(img, x, yMin + split))
+					if (test(img, x, split))
 						continue outer;
-				return boundingBoxRecursive0(img, bbr, split + 1, xMax, yMin, yMax) | boundingBoxRecursive0(img, bbr, xMin, split - 1, yMin, yMax);
+				return split;
 			}
 			step /= 2;
 		}
-		return bbr.add(new PreciseRectangle(xMin, xMax, yMin, yMax));
+		return -1;
 	}
-
-	// used to test a 3 pixel vertical line to determine if it is adjacent.
-	// Middle pixel must meet threshold, plus on of the two others
-	private static final boolean adjV(boolean[][] img, int x, int y) {
-		return ((test(img, x, y)) && ((test(img, x, y - 1)) || (test(img, x, y + 1))));
-	}
-
-	// used to test a 3 pixel horizontal line to determine if it is adjacent.
-	// Middle pixel must meet threshold, plus on of the two others
-	private static final boolean adjH(boolean[][] img, int x, int y) {
-		return ((test(img, x, y)) && ((test(img, x - 1, y)) || (test(img, x + 1, y))));
+	
+	private static final int splitV(final boolean[][] img, final int xMin, final int xMax, final int yMin, final int yMax) {
+		int step = nextPowerOf2(xMax - xMin);
+		while (step > 2) {
+			outer: for (int split = xMin + step / 2; split < xMax; split += step) {
+				for (int y = yMin; y < yMax; y++)
+					if (test(img, split, y))
+						continue outer;
+				return split;
+			}
+			step /= 2;
+		}
+		return -1;
 	}
 
 	private static final boolean test(boolean[][] img, int x, int y) {
