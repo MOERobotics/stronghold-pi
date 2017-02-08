699d830cce981ebacd411cfc305532d6cea2e380
mailmindlin <liamttlyrox@gmail.com>
2017-02-08 12:43:03 -0500

    Implemented RioPacket's

--- src/com/moe365/mopi/client/NoneFoundRioPacket.java
+++ src/com/moe365/mopi/client/NoneFoundRioPacket.java
@@ -1,19 +1,22 @@
 package com.moe365.mopi.client;
+
 import java.nio.ByteBuffer;
 
-public class HelloRioPacket implements RioPacket {
+public class NoneFoundRioPacket implements RioPacket {
+
 	@Override
 	public int getStatus() {
-		return RioPacket.STATUS_HELLO_WORLD;
+		return RioPacket.STATUS_NONE_FOUND;
 	}
-	
+
 	@Override
 	public int getLength() {
 		return 0;
 	}
-	
+
 	@Override
 	public void writeTo(ByteBuffer buffer) {
-		//Nop
+		
 	}
+
 }
--- src/com/moe365/mopi/client/RioPacket.java
+++ src/com/moe365/mopi/client/RioPacket.java
@@ -2,6 +2,42 @@ package com.moe365.mopi.client;
 import java.nio.ByteBuffer;
 
 public interface RioPacket {
+	
+	/**
+	 * Denotes a packet that should be ignored. No idea why we would need to use
+	 * this, though.
+	 */
+	public static final short STATUS_NOP = 0;
+	/**
+	 * Denotes a packet telling the Rio that no target(s) were found.
+	 */
+	public static final short STATUS_NONE_FOUND = 1;
+	/**
+	 * Denotes a packet telling the Rio that one target has been detected. The
+	 * position data MUST be included in the packet.
+	 */
+	public static final short STATUS_ONE_FOUND = 2;
+	/**
+	 * Denotes a packet telling the Rio that two or more targets have been
+	 * found. The position data of the two largest targets found (by area) MUST
+	 * be included in the packet.
+	 */
+	public static final short STATUS_TWO_FOUND = 3;
+	/**
+	 * A packet that contains the 
+	 */
+	public static final short STATUS_ERROR = 4;
+	/**
+	 * A packet that notifies the reciever that the sender has just connected.
+	 * If this packet is recieved, the reciever should reset its last-recieved packet id
+	 * to the id of this packet.
+	 */
+	public static final short STATUS_HELLO_WORLD = 5;
+	/**
+	 * Signals that the sender is terminating in an expected manner.
+	 */
+	public static final short STATUS_GOODBYE = 6;
+	
 	/**
 	 * Get the status code for this packet
 	 */
