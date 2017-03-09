8d7eee50165eba02256cb4b9d6c75fe01670b4b3
mailmindlin <liamttlyrox@gmail.com>
2017-03-09 16:28:42 -0500

    Fix odd NPE

--- src/com/moe365/mopi/Main.java
+++ src/com/moe365/mopi/Main.java
@@ -9,7 +9,7 @@ import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.lang.Thread.UncaughtExceptionHandler;
 import java.net.InetSocketAddress;
-import java.net.NetworkInterface;
+import java.util.Arrays;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,6 +20,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+
+import javax.imageio.ImageIO;
 
 import com.moe365.mopi.CommandLineParser.ParsedCommandLineArguments;
 import com.moe365.mopi.geom.Polygon;
@@ -167,7 +169,8 @@ public class Main {
 				default:
 					System.err.println("Unknown test '" + target + "'");
 			}
-			device.release();
+			if (device != null)
+				device.release();
 			System.exit(0);
 		}
 		
