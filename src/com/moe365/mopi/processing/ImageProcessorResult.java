27f2e92b7f124fc020591bee5ea0c87a1ecdf356
mailmindlin <mailmindlin@users.noreply.github.com>
2017-04-17 11:41:25 -0400

    Switch ImageProcessorResult from interface to class

--- src/com/moe365/mopi/processing/ImageProcessorResult.java
+++ src/com/moe365/mopi/processing/ImageProcessorResult.java
@@ -2,8 +2,26 @@ package com.moe365.mopi.processing;
 
 import au.edu.jcu.v4l4j.VideoFrame;
 
-public static interface ImageProcessorResult<R> {
-	VideoFrameMetadata frameOnMeta();
-	VideoFrameMetadata frameOffMeta();
-	R get();
+public class ImageProcessorResult<R> {
+	protected final VideoFrameMetadata onMeta;
+	protected final VideoFrameMetadata offMeta;
+	protected final R result;
+	
+	public ImageProcessorResult(VideoFrameMetadata onMeta, VideoFrameMetadata offMeta, R result) {
+		this.onMeta = onMeta;
+		this.offMeta = offMeta;
+		this.result = result;
+	}
+	
+	public VideoFrameMetadata frameOnMeta() {
+		return this.onMeta;
+	}
+	
+	public VideoFrameMetadata frameOffMeta() {
+		return this.offMeta;
+	}
+	
+	pubilc R get() {
+		return result;
+	}
 }
